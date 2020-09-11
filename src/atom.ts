6c2ea7589026548cc95a46c01f4f143e47f1a3f1
meris bahtijaragic <meris.bahti@gmail.com>
2020-09-11 15:46:05 +0200

    fix: add perf test and fix perf

--- src/atom.ts
+++ src/atom.ts
@@ -1,7 +1,6 @@
 import { Lens, get, set } from 'optics-ts'
 import { BehaviorSubject } from 'rxjs'
 import { map, distinctUntilChanged } from 'rxjs/operators'
-import convertObservableToBehaviorSubject from './convert-observable-to-behavior-subject'
 import equal from 'deep-equal'
 
 export type Atom<S> = {
@@ -16,23 +15,21 @@ export const atom = <S>(
   atom$ = new BehaviorSubject<S>(value),
 ): Atom<S> => {
   return {
-    subscribe: listener => {
-      atom$.subscribe(next => listener(next))
-    },
-    focus: <A>(optic: Lens<S, any, A>): Atom<A> => {
+    subscribe: listener => atom$.subscribe(value => listener(value)),
+    focus: optic => {
       const getter = get(optic)
       const latestAValue = getter(atom$.value)
-      const parentObserver = atom$.pipe(
-        map(getter),
-        distinctUntilChanged(equal),
-      )
-      const newSub = convertObservableToBehaviorSubject(
-        parentObserver,
-        latestAValue,
-      )
-      newSub.subscribe(next => {
+
+      const newSub = new BehaviorSubject(latestAValue)
+
+      newSub.pipe(distinctUntilChanged(equal)).subscribe(next => {
         atom$.next(set(optic)(next)(atom$.value))
       })
+
+      atom$
+        .pipe(map(getter), distinctUntilChanged(equal))
+        .subscribe(next => newSub.next(next))
+
       return atom(getter(atom$.value), newSub)
     },
     update: updater => {
