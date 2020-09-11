b2a985ae34729312538acb8cbdd85bc7d875a6dd
meris bahtijaragic <meris.bahti@gmail.com>
2020-09-11 13:34:56 +0200

    fix: export atom

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react'
+// import React, { FC, HTMLAttributes, ReactChild } from 'react'
+//
+// export interface Props extends HTMLAttributes<HTMLDivElement> {
+//   children?: ReactChild
+// }
+//
+// // Please do not use types off of a default export module or else Storybook Docs will suffer.
+// // see: https://github.com/storybookjs/storybook/issues/9556
+// export const Thing: FC<Props> = ({ children }) => {
+//   return <div>{children || `the snozzberries taste like snozzberries`}</div>
+// }
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  children?: ReactChild
-}
+import { atom } from './atom'
 
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>
-}
+export default atom
