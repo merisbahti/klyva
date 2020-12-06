2123ea49d7100ba7fecc878b10bccd3e4ae3034c
Meris Bahtijaragic <meris.bahti@gmail.com>
2020-12-06 08:55:03 +0100

    fix(lint): remove unused imports automatically.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,13 @@ module.exports = {
       "version": "detect"
     }
   },
+  "plugins": ["unused-imports"],
+  "rules": {
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports-ts": "error",
+    "unused-imports/no-unused-vars-ts": [
+      "warn",
+      { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+    ],
+  }
 }
