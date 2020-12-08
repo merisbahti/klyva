d562faceccaf1b65a7323ce0850e4601625ca128
Meris Bahtijaragic <meris.bahti@gmail.com>
2020-12-08 19:10:16 +0100

    fix(test): clean up atom-to-source-test

---
+++ test/atom-to-source.test.ts
@@ -0,0 +1,20 @@
+import subscribe from 'callbag-subscribe'
+import { atom } from '../src/atom'
+import { atomToSource } from '../src/atom-to-source'
+
+test('atomtosource', () => {
+  const numberAtom = atom(0)
+  const callbagSource = atomToSource(numberAtom)
+  let latestValue = null
+  subscribe(v => (latestValue = v))(callbagSource)
+  expect(latestValue).toBe(null)
+
+  numberAtom.update(1)
+  expect(latestValue).toBe(1)
+
+  numberAtom.update(2)
+  expect(latestValue).toBe(2)
+
+  numberAtom.update(3)
+  expect(latestValue).toBe(3)
+})
