87d33d19a1ffc56bc81fb470122ff5733e5759d0
Meris Bahtijaragic <meris.bahti@gmail.com>
2020-12-12 21:01:39 +0100

    fix(use-array-slice): add test asserting that updates of sibling atoms does not update other sibling atoms

--- test/use-array-slice.test.tsx
+++ test/use-array-slice.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import * as rtl from '@testing-library/react'
 import { atom } from '../src'
 import { PrimitiveAtom } from '../src/types'
-import { useAtomSlice, useSelector } from '../src/react-utils'
+import { useAtom, useAtomSlice, useSelector } from '../src/react-utils'
 
 it('useArraySlice removal works', async () => {
   const anAtom = atom(['foo', 'bar', 'baz'])
@@ -78,4 +78,61 @@ it('useArraySlice, filter, removal works', async () => {
 
   const bar = queryByText('bar')
   expect(bar).toBeNull()
+})
+
+const useUpdateCount = () => {
+  const count = React.useRef(0)
+  React.useEffect(() => {
+    count.current += 1
+  })
+  return count.current
+}
+
+it('useArraySlice, updating a sibling atom does not update other siblings', async () => {
+  const anAtom = atom([{ count: 0 }, { count: 0 }])
+  const CounterList = ({
+    myAtom,
+  }: {
+    myAtom: PrimitiveAtom<{ count: number }[]>
+  }) => {
+    const atoms = useAtomSlice(myAtom)
+    return (
+      <div>
+        {atoms.map((atom, id) => (
+          <Counter id={id} key={id} countAtom={atom} />
+        ))}
+      </div>
+    )
+  }
+
+  const Counter = ({
+    countAtom,
+    id,
+  }: {
+    id: number
+    countAtom: PrimitiveAtom<{ count: number }>
+  }) => {
+    const [count, setCount] = useAtom(countAtom)
+    const updates = useUpdateCount()
+    return (
+      <button key={id} onClick={() => setCount(v => ({ count: v.count + 1 }))}>
+        id: {id}, count: {count.count}, updates: {updates}
+      </button>
+    )
+  }
+
+  const { getByText, findByText /*, queryByText*/ } = rtl.render(
+    <CounterList myAtom={anAtom} />,
+  )
+
+  await findByText('id: 0, count: 0, updates: 0')
+  await findByText('id: 1, count: 0, updates: 0')
+
+  rtl.fireEvent.click(getByText('id: 0, count: 0, updates: 0'))
+  await findByText('id: 0, count: 1, updates: 1')
+  await findByText('id: 1, count: 0, updates: 0')
+
+  rtl.fireEvent.click(getByText('id: 1, count: 0, updates: 0'))
+  await findByText('id: 0, count: 1, updates: 1')
+  await findByText('id: 1, count: 1, updates: 1')
 })
