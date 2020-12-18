760bb7afb3a5ed025eb27d9b67b4ea981575a8bd
David <david@krawaller.se>
2020-12-18 20:14:48 +0100

    comment atomcheckbox

--- examples/todo/components/AtomCheckBox.tsx
+++ examples/todo/components/AtomCheckBox.tsx
@@ -2,6 +2,12 @@ import * as React from 'react'
 import { useAtom } from 'klyva'
 import { PrimitiveAtom } from 'klyva/dist/types'
 
+/*
+A generic component that connects a bool atom to a checkbox.
+Not specific to TodoMVC, could conceivably be included in a 
+generic Klyva React UI toolkit.
+*/
+
 type AtomCheckBoxProps = {
   boolAtom: PrimitiveAtom<boolean>
 } & React.HTMLAttributes<HTMLInputElement>
