a9a880bb362476300fcf8b731654966b86fea724
Meris Bahtijaragic <meris.bahti@gmail.com>
2020-12-21 08:56:36 +0100

    fix(types): remove unused types

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,17 +1,1 @@
 // adds types to other packages
-
-declare module 'callbag-tap' {
-  import { Source } from 'callbag'
-
-  const tap: <T>(cb: (value: T) => void) => (source: Source<T>) => Source<T>
-  export default tap
-}
-
-declare module 'callbag-concat-map' {
-  import { Source } from 'callbag'
-
-  const concatMap: <T, B>(
-    cb: (value: T) => Source<B>,
-  ) => (source: Source<T>) => Source<B>
-  export default concatMap
-}
