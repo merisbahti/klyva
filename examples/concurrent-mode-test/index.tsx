fe881f596f26b0fc3143eb4f3bbb760ddfc05867
Meris Bahtijaragic <meris.bahti@gmail.com>
2021-04-06 16:40:41 +0200

    fix: start work with adding CM tests

---
+++ examples/concurrent-mode-test/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { atom, useAtom } from 'klyva'
+import {
+  createApp,
+  doubleAction,
+  incrementAction,
+  initialState,
+  reducer,
+} from './create-app'
+
+const stateAtom = atom(initialState)
+
+const useCount = () => useAtom(stateAtom)[0].count
+
+const useIncrement = () => () =>
+  stateAtom.update(value => reducer(value, incrementAction))
+
+const useDouble = () => () =>
+  stateAtom.update(value => reducer(value, doubleAction))
+
+stateAtom.subscribe(console.log)
+
+const App = createApp(useCount, useIncrement, useDouble)
+
+const rootElement = document.getElementById('root')
+const root = rootElement && ReactDOM.unstable_createRoot(rootElement)
+root?.render(<App />)
--- src/react-utils.ts
+++ src/react-utils.ts
@@ -2,7 +2,6 @@ import React from 'react'
 import { unstable_batchedUpdates } from 'react-dom'
 import { atom } from './atom'
 import { Atom, CustomAtom, ReadableAtom, RemovableAtom, Updater } from './'
-import equal from './equal'
 
 export function useAtom<Value>(
   atom: Atom<Value>,
@@ -14,16 +13,9 @@ export function useAtom<Value>(atom: ReadableAtom<Value>): [Value]
 export function useAtom<Value, Updater = unknown>(
   atom: ReadableAtom<Value> & { update?: (updater: Updater) => void },
 ) {
-  const [cache, setCache] = React.useState(atom.getValue)
+  const [, forceUpdate] = React.useReducer(() => [], [])
   React.useEffect(() => {
-    setCache(oldCache => {
-      const currValue = atom.getValue()
-      if (equal(currValue, oldCache)) {
-        return oldCache
-      }
-      return currValue
-    })
-    const unsub = atom.subscribe(value => setCache(() => value))
+    const unsub = atom.subscribe(forceUpdate)
     return unsub
   }, [atom])
   const updaterMaybe: null | ((updater: Updater) => void) = React.useMemo(
@@ -37,7 +29,8 @@ export function useAtom<Value, Updater = unknown>(
         : null,
     [atom],
   )
-  return [cache, ...(updaterMaybe ? [updaterMaybe] : [])]
+  console.log('fart')
+  return [atom.getValue(), ...(updaterMaybe ? [updaterMaybe] : [])]
 }
 
 type UseSelector = {
